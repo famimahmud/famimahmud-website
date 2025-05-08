7beb2a8b5b8d75881d3b3c505aafe660f0045d2d
Fami <mahmudfami@gmail.com>
2025-05-08 11:43:11 +0200

    Tailwind Setup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React World from Fami Mahmud!</h1>
+    <div className="min-h-screen bg-blue-100 text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-800">🚀 Tailwind is setup!</h1>
     </div>
   );
 }
